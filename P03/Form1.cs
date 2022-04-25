caa98efdc7c6fb259ac61efa19e82eee1f6fab5f
Vít Novotný <novotvit@hotmail.com>
2022-04-25 09:34:58 +0200

    Začátek P03

--- P03/Form1.cs
+++ P03/Form1.cs
@@ -16,5 +16,19 @@ namespace P03
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int c = Convert.ToInt32(textBox3.Text);
+            if (a != string.Empty)
+            {
+
+            }else if (a != string.Empty && b != string.Empty)
+            {
+
+            }
+        }
     }
 }
