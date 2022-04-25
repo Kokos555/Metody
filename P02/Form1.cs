8be2b01c15feb059cbb0ba5203de92f15fabed3b
Vít Novotný <novotvit@hotmail.com>
2022-04-25 09:27:11 +0200

    PoleUtil vytvoření a implementace do programu.

--- P02/Form1.cs
+++ P02/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PoleUtil;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,62 +18,7 @@ namespace P02
             InitializeComponent();
         }
 
-        private int[] Generovani(int pocet, int dolni_mez = 1, int horni_mez = 100)
-        {
-            Random nh = new Random();
-            int[] pole = new int[pocet];
-            for (int i = 0; i < pocet; i++) {
-                int x = nh.Next(dolni_mez, horni_mez);
-                pole[i] = x;
-            }
-            return pole;
-        }
 
-
-        private void Zobrazit(ListBox cisla, int[] pole)
-        {
-            cisla.Items.Clear();
-            for (int i = 0; i < pole.Length; i++) {
-                cisla.Items.Add(pole[i]);
-            }
-        }
-
-        private int SoucetAPocet(out int pocet, int[] pole)
-        {
-            pocet = 0;
-            int soucet = 0;
-            foreach (int cisla in pole)
-            {
-                if (cisla % 2 == 0)
-                {
-                    soucet += cisla;
-                } else
-                {
-                    pocet++;
-                }
-            }
-            return soucet;
-        }
-        private bool Roustouci(int[] pole)
-        {
-            for (int i = 1; i < pole.Length; i++)
-            {
-                if (pole[i] <= pole[i - 1])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        private void Vymena(int[] pole)
-        {
-            int max = pole.Max();
-            int poradi = Array.IndexOf(pole, max);
-            int posledni_radek_pole = pole.Length - 1;
-            pole[poradi] = pole[posledni_radek_pole];
-            pole[posledni_radek_pole] = max;
-        }
         private void button1_Click(object sender, EventArgs e)
         {
             int[] pole;
@@ -80,18 +26,18 @@ namespace P02
             if (textBox2.Text != "" && textBox3.Text != "") { 
                 int horni_mez = Convert.ToInt32(textBox3.Text);
                 int dolni_mez = Convert.ToInt32(textBox2.Text);
-                pole = Generovani(pocet, dolni_mez, horni_mez);
+                pole = Pole.Generovani(pocet, dolni_mez, horni_mez);
             }else
             {
-                pole = Generovani(pocet);
+                pole = Pole.Generovani(pocet);
             }
-            Zobrazit(listBox1, pole);
+            Pole.Zobrazit(listBox1, pole);
             int pocet2;
-            int soucet_sudych = SoucetAPocet(out pocet2, pole);
+            int soucet_sudych = Pole.SoucetAPocet(out pocet2, pole);
             MessageBox.Show(string.Format("Součet sudých čísel je {0} a počet lichých čísel je {1}", soucet_sudych, pocet2));
-            MessageBox.Show(string.Format("Toto generovani cisel {0} roustoucí.", Roustouci(pole) ? "je" : "není"));
-            Vymena(pole);
-            Zobrazit(listBox2, pole);
+            MessageBox.Show(string.Format("Toto generovani cisel {0} roustoucí.", Pole.Roustouci(pole) ? "je" : "není"));
+            Pole.Vymena(pole);
+            Pole.Zobrazit(listBox2, pole);
 
         }
     }
---
+++ PoleUtil/Pole.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PoleUtil
+{
+    public class Pole
+    {
+        /// <summary>
+        /// Generovaní čísel do pole.
+        /// </summary>
+        /// <param name="pocet">Počet prvků </param>
+        /// <param name="dolni_mez">Dolní interval</param>
+        /// <param name="horni_mez">Horní internval</param>
+        /// <returns>Vygenerované pole prvnků</returns>
+        static public int[] Generovani(int pocet, int dolni_mez = 1, int horni_mez = 100)
+        {
+            Random nh = new Random();
+            int[] pole = new int[pocet];
+            for (int i = 0; i < pocet; i++)
+            {
+                int x = nh.Next(dolni_mez, horni_mez);
+                pole[i] = x;
+            }
+            return pole;
+        }
+
+        /// <summary>
+        /// Zobrazení do listboxu.
+        /// </summary>
+        /// <param name="cisla">Jméno listboxu</param>
+        /// <param name="pole">Pole vygenerovaných čísel</param>
+        static public void Zobrazit(ListBox cisla, int[] pole)
+        {
+            cisla.Items.Clear();
+            for (int i = 0; i < pole.Length; i++)
+            {
+                cisla.Items.Add(pole[i]);
+            }
+        }
+        /// <summary>
+        /// Součet sudých čísel a počet lichých čísel
+        /// </summary>
+        /// <param name="pocet">Počet pro lichá čísla</param>
+        /// <param name="pole">Pole vygenerovaných čísel</param>
+        /// <returns>Součet sudých čísel a počet lichých</returns>
+        static public int SoucetAPocet(out int pocet, int[] pole)
+        {
+            pocet = 0;
+            int soucet = 0;
+            foreach (int cisla in pole)
+            {
+                if (cisla % 2 == 0)
+                {
+                    soucet += cisla;
+                }
+                else
+                {
+                    pocet++;
+                }
+            }
+            return soucet;
+        }
+        /// <summary>
+        /// Zjištění jestli je posloupnost rostoucí.
+        /// </summary>
+        /// <param name="pole">Pole vygenerovaných čísel</param>
+        /// <returns>Rostoucí posloupnost</returns>
+        static public bool Roustouci(int[] pole)
+        {
+            for (int i = 1; i < pole.Length; i++)
+            {
+                if (pole[i] <= pole[i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// Výměna maxima s posledním prvkem pole.
+        /// </summary>
+        /// <param name="pole">Pole vygenerovaných čísel</param>
+        static public void Vymena(int[] pole)
+        {
+            int max = pole.Max();
+            int poradi = Array.IndexOf(pole, max);
+            int posledni_radek_pole = pole.Length - 1;
+            pole[poradi] = pole[posledni_radek_pole];
+            pole[posledni_radek_pole] = max;
+        }
+    }
+}
