0e4e2009175f20c702275254cf0840b4be1b9aad
Vít Novotný <novotvit@hotmail.com>
2022-04-25 08:47:16 +0200

    Vytvoření a implementovaní knihovny Posloupnost

--- P01/Form1.cs
+++ P01/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PosloupnostUtil;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,23 +17,12 @@ namespace P01
         {
             InitializeComponent();
         }
-        static private int SoucetArPosl(ref int a1, int a2, int n, out int diference) {
-            diference = a2 - a1;
-            int soucet = 0;
-            for (int i = 0; i < n - 1; i++)
-            {
-                soucet += a1;
-                a1 += diference;
-            }
-            soucet += a1;
-            return soucet;
-        }
         private void button1_Click(object sender, EventArgs e)
         {
             int a1 = Convert.ToInt32(textBox1.Text);
             int a2 = Convert.ToInt32(textBox2.Text);
             int n = Convert.ToInt32(textBox3.Text);
-            int vysledek = SoucetArPosl(ref a1, a2, n,out int diference);
+            int vysledek = Posloupnost.SoucetArPosl(ref a1, a2, n, out int diference);
             MessageBox.Show(string.Format("{0} posledni prvek je {1} a diference posloupnosti je {2}",vysledek ,a1,diference));
         }
     }
---
+++ PosloupnostUtil/Posloupnost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PosloupnostUtil
+{
+    public class Posloupnost
+    {
+        /// <summary>
+        /// Součet aritmetické posloupnosti
+        /// </summary>
+        /// <param name="a1">První člen</param>
+        /// <param name="a2">Druhý člen</param>
+        /// <param name="n">Počet členů</param>
+        /// <param name="diference">Rozdíl a2 - a1</param>
+        /// <returns>Součet n členů arimetické posloupnosti</returns>
+        static public int SoucetArPosl(ref int a1, int a2, int n, out int diference)
+        {
+            diference = a2 - a1;
+            int soucet = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                soucet += a1;
+                a1 += diference;
+            }
+            soucet += a1;
+            return soucet;
+        }
+    }
+}
